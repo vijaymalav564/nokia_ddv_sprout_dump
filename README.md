112ca6182d5e637463e0694f9934f4faa6237260
vijaymalav564 <jaymalav10@gmail.com>
2022-01-03 13:33:12 +0000

    Add extras for m690-user 11 RKQ1.210607.001 00WW_3_440 release-keys
    Signed-off-by: vijaymalav564 <jaymalav10@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## m690-user 11 RKQ1.210607.001 00WW_3_440 release-keys
+- Manufacturer: hmd global
+- Platform: sdm660
+- Codename: DDV_sprout
+- Brand: Nokia
+- Flavor: m690-user
+- Release Version: 11
+- Id: RKQ1.210607.001
+- Incremental: 00WW_3_440
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Locale: en-GB
+- Screen Density: undefined
+- Fingerprint: Nokia/Daredevil_00WW/DDV_sprout:11/RKQ1.210607.001/00WW_3_440:user/release-keys
+- OTA version: 
+- Branch: m690-user-11-RKQ1.210607.001-00WW_3_440-release-keys
+- Repo: nokia_ddv_sprout_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/priv-app/HMDCamera/HMDCamera.apk.* 2>/dev/null >> system/system/priv-app/HMDCamera/HMDCamera.apk
+rm -f system/system/priv-app/HMDCamera/HMDCamera.apk.* 2>/dev/null
+cat system/system/product/app/Photos/Photos.apk.* 2>/dev/null >> system/system/product/app/Photos/Photos.apk
+rm -f system/system/product/app/Photos/Photos.apk.* 2>/dev/null
+cat system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat system/system/product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> system/system/product/app/Gmail2/Gmail2.apk
+rm -f system/system/product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null >> system/system/product/app/YouTube/YouTube.apk
+rm -f system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat system/system/product/app/Maps/Maps.apk.* 2>/dev/null >> system/system/product/app/Maps/Maps.apk
+rm -f system/system/product/app/Maps/Maps.apk.* 2>/dev/null
+cat system/system/product/app/Messages/Messages.apk.* 2>/dev/null >> system/system/product/app/Messages/Messages.apk
+rm -f system/system/product/app/Messages/Messages.apk.* 2>/dev/null
+cat system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
